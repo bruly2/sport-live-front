1a3ce7d019586413f9c9b768b0283dc883b7d638
bruly2 <b.bertogal@gmail.com>
2023-08-21 14:50:53 +0200

    Add HTML: Header connected + Footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import "./footer.scss";
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer>
+            <hr />
+            <Link to="/donnees-personnelles">Données personnelles</Link>
+            <Link to="/mentions-legales">Mentions légales</Link>
+            <Link to="/contact">Contact</Link>
+        </footer>
+    );
 };
 
 export default Footer;
