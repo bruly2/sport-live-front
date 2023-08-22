34532a004e44688a0e609d2731605c42148b09e9
bruly2 <b.bertogal@gmail.com>
2023-08-22 10:29:17 +0200

    Dynamic display scrolling message

--- src/components/ScrollingText/ScrollingText.tsx
+++ src/components/ScrollingText/ScrollingText.tsx
@@ -1,10 +1,43 @@
 import "./scrollingtext.scss";
+import { useState } from "react";
+
+const messages = [
+    {
+        user: "Christophe",
+        content: "1er message",
+    },
+    {
+        user: "Kévin",
+        content: "2e message",
+    },
+    {
+        user: "Thomas",
+        content: "3e message",
+    },
+    {
+        user: "Bruno",
+        content: "4e message",
+    },
+];
 
 const ScrollingText = () => {
+    const [displayMessage, setDisplayMessage] = useState([]);
+
+    // Affichage du 1er message
+    const createMessage = () => {
+        setDisplayMessage(messages[0].content);
+    };
+
     return (
-        <div id="scrolling-text">
-            <span>Défilement des messages</span>
-        </div>
+        <>
+            <div id="scrolling-text">
+                <span>{displayMessage}</span>
+            </div>
+            {/* Bouton provisoire */}
+            <button className="btn-secondary" onClick={createMessage}>
+                Afficher un message
+            </button>
+        </>
     );
 };
 export default ScrollingText;
