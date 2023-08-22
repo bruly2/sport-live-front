3a1b2504955c927de82296296d0aa1afd07f0cf5
christophehascoet <yoda19712022@outlook.fr>
2023-08-22 16:22:47 +0200

    scroll

--- src/components/Event/ScrollingText/ScrollingText.tsx
+++ src/components/Event/ScrollingText/ScrollingText.tsx
@@ -1,10 +1,23 @@
 import "./scrollingtext.scss";
-
+import { useState } from "react";
 const ScrollingText = () => {
-    return (
-        <div id="scrolling-text">
-            <span>Défilement des messages</span>
-        </div>
+  const [currentMessage, setCurrentMessage] = useState([]);
+  const scrollMessage = (offset: number) => {
+    setCurrentMessageIndex((prevIndex) =>
+      (prevIndex + offset + messages.length) % messages.length
     );
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => scrollMessage(1), 3000); 
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  return (
+    <div id="scrolling-text">
+      <span>Défilement des messages{messages[currentMessageIndex]?.content}</span>
+    </div>
+  );
 };
 export default ScrollingText;
