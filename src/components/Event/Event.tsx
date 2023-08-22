ba0f4fe9a3226328128443d3bc4c792aed0e8c9e
bruly2 <b.bertogal@gmail.com>
2023-08-22 09:54:59 +0200

    Move SrcollingText in components

--- src/components/Event/Event.tsx
+++ src/components/Event/Event.tsx
@@ -1,5 +1,4 @@
 import "./Event.scss";
-import ScrollingText from "./ScrollingText/ScrollingText";
 
 export const Event = () => {
     return (
@@ -16,7 +15,6 @@ export const Event = () => {
                     </p>
                 </div>
             </main>
-            <ScrollingText />
         </>
     );
 };
--- src/pages/Hub/Hub.tsx
+++ src/pages/Hub/Hub.tsx
@@ -1,11 +1,14 @@
 import Event from "../../components/Event/Event";
 import Card from "../../components/Card/Card";
+import ScrollingText from "../../components/ScrollingText/ScrollingText";
+
 import "./hub.scss";
 
 const Hub = () => {
     return (
         <>
             <Event />
+            <ScrollingText />
             <section>
                 <Card title={"Message"} img={"/sport-live-copy.png"} />
                 <Card title={"Sondage"} img={"/sport-live-copy.png"} />
