00e9446179508b31b4c87361dc47fc373eeac0a0
christophehascoet <yoda19712022@outlook.fr>
2023-08-23 10:07:44 +0200

    scrolling

---
+++ src/components/ScrollingText/ScrollingText.tsx
@@ -0,0 +1,42 @@
+// import "./scrollingtext.scss";
+// import { useState } from "react";
+// import Button from "../Button/Button";
+
+// const messages = [
+//     {
+//         user: "Christophe",
+//         content: "1er message",
+//     },
+//     {
+//         user: "Kévin",
+//         content: "2e message",
+//     },
+//     {
+//         user: "Thomas",
+//         content: "3e message",
+//     },
+//     {
+//         user: "Bruno",
+//         content: "4e message",
+//     },
+// ];
+
+// const ScrollingText = () => {
+//     const [displayMessage, setDisplayMessage] = useState([]);
+
+//     // Affichage du 1er message
+//     const createMessage = () => {
+//         setDisplayMessage(messages[0].content);
+//     };
+
+//     return (
+//         <>
+//             <div id="scrolling-text">
+//                 <span>{displayMessage}</span>
+//             </div>
+//             {/* Bouton provisoire */}
+//             <Button className={"btn-secondary"} onClick={createMessage} content={"Afficher un message"} />            
+//         </>
+//     );
+// };
+// export default ScrollingText;
