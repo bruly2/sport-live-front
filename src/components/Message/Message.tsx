252f1b1053506ab986d3d3898387cfd4c399a90f
christophehascoet <yoda19712022@outlook.fr>
2023-08-23 16:24:54 +0200

    creation

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,75 +1,70 @@
 import "./message.scss";
 import { useState } from "react";
 import Button from "../Button/Button";
-import { IoIosClose } from "react-icons/Io";
+import { IoIosClose } from "react-icons/io";
 
 const Message = ({ closeBigCard }) => {
-    const [textArea, setTextArea] = useState("");
+  const [textArea, setTextArea] = useState("");
 
-    const [errorForm, setErrorForm] = useState("");
+  const [errorForm, setErrorForm] = useState("");
 
-    const [showConfirmation, setShowConfirmation] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
-    const handleTest = (e) => {
-        setTextArea(e.target.value);
-    };
+  const handleTest = (e) => {
+    setTextArea(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        checkForm();
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    checkForm();
+  };
 
-    const checkForm = () => {
-        // Form vide
-        if (textArea === "") {
-            return setErrorForm("Tapez votre texte pour envoyer un message");
-        }
-        // Form trop court
-        else if (textArea.length < 3) {
-            return setErrorForm("Complete ton message");
-        }
+  const checkForm = () => {
+    // Form vide
+    if (textArea === "") {
+      return setErrorForm("Tapez votre texte pour envoyer un message");
+    }
+    // Form trop court
+    else if (textArea.length < 3) {
+      return setErrorForm("Complete ton message");
+    }
 
-        // => Formulaire validé
-        setShowConfirmation(true);
-        console.log(textArea);
-    };
+    // => Formulaire validé
+    setShowConfirmation(true);
+    console.log(textArea);
+  };
 
-    return (
-        <>
-            <article id="message">
-                <Button
-                    type={"button"}
-                    className={"close"}
-                    onClick={closeBigCard}
-                >
-                    <span>
-                        <IoIosClose />
-                    </span>
-                </Button>
-                <h2>
-                    Surveillez l'écran du stade pour voir votre message en
-                    direct&nbsp;!
-                </h2>
+  return (
+    <>
+      <article id="message">
+        <Button type={"button"} className={"close"} onClick={closeBigCard}>
+          <span>
+            <IoIosClose />
+          </span>
+        </Button>
+        <h2>
+          Surveillez l'écran du stade pour voir votre message en direct&nbsp;!
+        </h2>
 
-                {!showConfirmation ? (
-                    <form onSubmit={handleSubmit}>
-                        <textarea
-                            name="message-content"
-                            id="message-content"
-                            placeholder="Go Spurs Go !!"
-                            value={textArea}
-                            onChange={(e) => handleTest(e)}
-                        ></textarea>
-                        <p className="errorform">{errorForm}</p>
-                        <Button type={"submit"} className={"btn-primary-2"}>
-                            Valider
-                        </Button>
-                    </form>
-                ) : (
-                    <h1>Message envoyé</h1>
-                )}
-            </article>
-        </>
-    );
+        {!showConfirmation ? (
+          <form onSubmit={handleSubmit}>
+            <textarea
+              name="message-content"
+              id="message-content"
+              placeholder="Go Spurs Go !!"
+              value={textArea}
+              onChange={(e) => handleTest(e)}
+            ></textarea>
+            <p className="errorform">{errorForm}</p>
+            <Button type={"submit"} className={"btn-primary-2"}>
+              Valider
+            </Button>
+          </form>
+        ) : (
+          <h1>Message envoyé</h1>
+        )}
+      </article>
+    </>
+  );
 };
 export default Message;
--- src/components/Poll/Poll.tsx
+++ src/components/Poll/Poll.tsx
@@ -1,22 +1,29 @@
 import Button from "../Button/Button";
-import { IoIosClose } from "react-icons/Io";
+import { IoIosClose } from "react-icons/io";
+
 import "./poll.scss";
+
 const Poll = ({ closeBigCard }) => {
-    return (
-        <>
-            <article id="poll">
-                <h1>Poll</h1>
-                <Button
-                    type={"button"}
-                    className={"close"}
-                    onClick={closeBigCard}
-                >
-                    <span>
-                        <IoIosClose />
-                    </span>
-                </Button>
-            </article>
-        </>
-    );
+  return (
+    <>
+      <article id="poll">
+        <h1>Poll</h1>
+        <Button type={"button"} className={"close"} onClick={closeBigCard}>
+          <span>
+            <IoIosClose />
+          </span>
+        </Button>
+
+        <h2>Posez votre question poour le sondage ?</h2>
+        <ul>
+          <li>choix</li>
+          <li>choix</li>
+          <li>choix</li>
+          <li>choix</li>
+        </ul>
+        <button type="button">Voter</button>
+      </article>
+    </>
+  );
 };
 export default Poll;
