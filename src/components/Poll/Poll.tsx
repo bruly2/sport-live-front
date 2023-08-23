460bb603606d7907985ef71d392120fd85d6cdeb
bruly2 <b.bertogal@gmail.com>
2023-08-23 19:53:37 +0200

    Update Poll

--- src/components/Poll/Poll.tsx
+++ src/components/Poll/Poll.tsx
@@ -4,26 +4,32 @@ import { IoIosClose } from "react-icons/io";
 import "./poll.scss";
 
 const Poll = ({ closeBigCard }) => {
-  return (
-    <>
-      <article id="poll">
-        <h1>Poll</h1>
-        <Button type={"button"} className={"close"} onClick={closeBigCard}>
-          <span>
-            <IoIosClose />
-          </span>
-        </Button>
+    return (
+        <>
+            <article id="poll">
+                <h1>Poll</h1>
+                <Button
+                    type={"button"}
+                    className={"close"}
+                    onClick={closeBigCard}
+                >
+                    <span>
+                        <IoIosClose />
+                    </span>
+                </Button>
 
-        <h2>Posez votre question poour le sondage ?</h2>
-        <ul>
-          <li>choix</li>
-          <li>choix</li>
-          <li>choix</li>
-          <li>choix</li>
-        </ul>
-        <button type="button">Voter</button>
-      </article>
-    </>
-  );
+                <h2>Posez votre question poour le sondage ?</h2>
+                <ul>
+                    <li>choix</li>
+                    <li>choix</li>
+                    <li>choix</li>
+                    <li>choix</li>
+                </ul>
+                <Button type={"button"} className={"btn-primary-2"}>
+                    Voter
+                </Button>
+            </article>
+        </>
+    );
 };
 export default Poll;
