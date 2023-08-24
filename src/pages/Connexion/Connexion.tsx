99dec96dcc408e58ff533824feb6d506ce267296
christophehascoet <yoda19712022@outlook.fr>
2023-08-24 18:36:08 +0200

    creation of the registration and login form

--- src/pages/Connexion/Connexion.tsx
+++ src/pages/Connexion/Connexion.tsx
@@ -1,4 +1,33 @@
+ import "./Connexion.scss";
+import Button from "../../components/Button/Button"
 const Connexion = () => {
-    return <h1>Connexion</h1>;
+    return (
+        <>
+          <div>
+            <h1>Connexion</h1>
+            <p>Participer à votre évènement</p>
+    
+            <form action="">
+              
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" />
+              
+                <label htmlFor="password">Mot de passe</label>
+                <input type="password" id="password" name="password"/>
+
+                <Button type={"submit"} className={"btn-primary-2"}>
+                            se connecter
+            </Button>
+            </form>
+          </div>
+
+          <div className ="form-compte">
+            <p>Pas encore de compte </p>
+            <Button type={"submit"} className={"btn-primary-2"}>
+                            creer un compte
+            </Button>
+          </div>
+        </>
+      );
 };
 export default Connexion;
--- src/pages/Inscription/Inscription.tsx
+++ src/pages/Inscription/Inscription.tsx
@@ -1,8 +1,36 @@
+import "./Inscription.scss";
+import Button from "../../components/Button/Button";
+
 const Inscription = () => {
-    return (
-        <>
-            <h1>Inscription</h1>
-        </>
-    );
+  
+  return (
+    <>
+      <div>
+        <h1>création d'un compte utilisateur</h1>
+        <p>En 30 seconde seulement</p>
+
+        <form action="">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" />
+
+          <label htmlFor="prenom">Prénom</label>
+          <input type="text" id="prenom" name="prenom" />
+
+          <label htmlFor="nom">Nom</label>
+          <input type="text" id="nom" name="nom" />
+
+          <label htmlFor="password">création mot de passe</label>
+          <input type="password" id="password" name="password" />
+
+          <label htmlFor="password">confirmation du mot de passe</label>
+          <input type="password" id="password" name="passwor" />
+
+          <Button type={"submit"} className={"btn-primary-2"}>
+            creer mon compte
+          </Button>
+        </form>
+      </div>
+    </>
+  );
 };
 export default Inscription;
