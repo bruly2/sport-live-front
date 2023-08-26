f5d449c21a2585ee39d0dabaee31af80979e2967
bruly2 <b.bertogal@gmail.com>
2023-08-26 03:45:38 +0200

    Install fetch Inscription

--- src/pages/Connexion/Connexion.jsx
+++ src/pages/Connexion/Connexion.jsx
@@ -1,4 +1,93 @@
+import { useForm } from "react-hook-form";
+import "./connexion.scss";
+import Button from "../../components/Button/Button";
+
 const Connexion = () => {
-    return <h1>Connexion</h1>;
+    // FETCH
+    const inscriptionFetch = async (data) => {
+        console.log(data);
+        try {
+            const response = await fetch(
+                "http://localhost:8000/api/users/register",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(data),
+                }
+            );
+            const result = await response.json();
+            console.log(
+                `🥳🥳 Utilisateur : ${result.firstname} enregistré en BDD !! 🥳🥳`
+            );
+        } catch (error) {
+            console.error("❌ Erreur ❌");
+        }
+    };
+
+    // FORM
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors, isSubmitSuccessful },
+    } = useForm({ mode: "onTouched" });
+    const onSubmit = (data) => inscriptionFetch(data);
+
+    return (
+        <>
+            {isSubmitSuccessful ? (
+                <>
+                    <h1>{watch("alias")}, votre compte est crée 🥳</h1>
+                    <h2>TODO : Redirection vers la page de Connexion</h2>
+                    {/* TODO Redirection vers la page connexion */}
+                </>
+            ) : (
+                <>
+                    <h1>Connexion</h1>
+                    <h2>Participer à votre événement</h2>
+                    <form
+                        className="connexion"
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        {/* PSEUDO */}
+                        <label htmlFor="alias">Pseudo</label>
+                        <input
+                            type="text"
+                            placeholder="pseudo"
+                            {...register("alias", {
+                                required: "Champs obligatoire",
+                            })}
+                        />
+                        {errors.alias && (
+                            <span className="error-form">
+                                {errors.alias.message}
+                            </span>
+                        )}
+
+                        {/* MOT DE PASSE */}
+                        <label htmlFor="password">Mot de passe</label>
+                        <input
+                            type="password"
+                            placeholder="mot de passe"
+                            {...register("password", {
+                                required: "Mot de passe obligatoire",
+                            })}
+                        />
+                        {errors.password && (
+                            <span className="error-form">
+                                {errors.password.message}
+                            </span>
+                        )}
+
+                        <Button type={"submit"} className={"btn-primary-2"}>
+                            Valider
+                        </Button>
+                    </form>
+                </>
+            )}
+        </>
+    );
 };
 export default Connexion;
--- src/pages/Inscription/Inscription.jsx
+++ src/pages/Inscription/Inscription.jsx
@@ -3,127 +3,171 @@ import "./inscription.scss";
 import Button from "../../components/Button/Button";
 
 const Inscription = () => {
+    // FETCH
+    const inscriptionFetch = async (data) => {
+        console.log(data);
+        try {
+            const response = await fetch(
+                "http://localhost:8000/api/users/register",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(data),
+                }
+            );
+            const result = await response.json();
+            console.log(
+                `🥳🥳 Utilisateur : ${result.firstname} enregistré en BDD !! 🥳🥳`
+            );
+        } catch (error) {
+            console.error("❌ Erreur ❌");
+        }
+    };
+
+    // FORM
     const {
         register,
         handleSubmit,
         watch,
         formState: { errors, isSubmitSuccessful },
     } = useForm({ mode: "onTouched" });
-    const onSubmit = (data) => console.log(data.pseudo);
+    const onSubmit = (data) => inscriptionFetch(data);
+
     return (
         <>
-            <h1>Création d'un compte</h1>
-            <h2>En 30 secondes seulement</h2>
-            <form className="connexion" onSubmit={handleSubmit(onSubmit)}>
-                {/* PSEUDO */}
-                <label htmlFor="alias">Pseudo</label>
-                <input
-                    type="text"
-                    placeholder="pseudo"
-                    {...register("alias", {
-                        required: "Champs obligatoire",
-                        minLength: {
-                            value: 3,
-                            message: "3 carateres minium",
-                        },
-                    })}
-                />
-                {errors.alias && (
-                    <span className="error-form">{errors.alias.message}</span>
-                )}
+            {isSubmitSuccessful ? (
+                <>
+                    <h1>{watch("alias")}, votre compte est crée 🥳</h1>
+                    <h2>TODO : Redirection vers la page de Connexion</h2>
+                    {/* TODO Redirection vers la page connexion */}
+                </>
+            ) : (
+                <>
+                    <h1>Création d'un compte</h1>
+                    <h2>En 30 secondes seulement</h2>
+                    <form
+                        className="connexion"
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        {/* PSEUDO */}
+                        <label htmlFor="alias">Pseudo</label>
+                        <input
+                            type="text"
+                            placeholder="pseudo"
+                            {...register("alias", {
+                                required: "Champs obligatoire",
+                                minLength: {
+                                    value: 3,
+                                    message: "3 carateres minium",
+                                },
+                            })}
+                        />
+                        {errors.alias && (
+                            <span className="error-form">
+                                {errors.alias.message}
+                            </span>
+                        )}
 
-                {/* PRENOM */}
-                <label htmlFor="prenom">Prénom</label>
-                <input
-                    type="text"
-                    placeholder="Prénom"
-                    {...register("prenom", {
-                        required: "Prénom obligatoire",
-                    })}
-                />
-                {errors.prenom && (
-                    <span className="error-form">{errors.prenom.message}</span>
-                )}
+                        {/* PRENOM */}
+                        <label htmlFor="firstname">Prénom</label>
+                        <input
+                            type="text"
+                            placeholder="Prénom"
+                            {...register("firstname", {
+                                required: "Prénom obligatoire",
+                            })}
+                        />
+                        {errors.firstname && (
+                            <span className="error-form">
+                                {errors.firstname.message}
+                            </span>
+                        )}
 
-                {/* NOM */}
-                <label htmlFor="nom">Nom</label>
-                <input
-                    type="text"
-                    placeholder="nom"
-                    {...register("nom", {
-                        required: "Nom obligatoire",
-                    })}
-                />
-                {errors.nom && (
-                    <span className="error-form">{errors.nom.message}</span>
-                )}
+                        {/* NOM */}
+                        <label htmlFor="lastname">Nom</label>
+                        <input
+                            type="text"
+                            placeholder="nom"
+                            {...register("lastname", {
+                                required: "Nom obligatoire",
+                            })}
+                        />
+                        {errors.lastname && (
+                            <span className="error-form">
+                                {errors.lastname.message}
+                            </span>
+                        )}
 
-                {/* Email 
+                        {/* Email 
                 TODO : Compléter la vérif email */}
-                <label htmlFor="nom">Email</label>
-                <input
-                    type="email"
-                    placeholder="email"
-                    {...register("email", {
-                        required: "Email obligatoire",
-                    })}
-                />
-                {errors.email && (
-                    <span className="error-form">{errors.email.message}</span>
-                )}
+                        <label htmlFor="nom">Email</label>
+                        <input
+                            type="email"
+                            placeholder="email"
+                            {...register("email", {
+                                required: "Email obligatoire",
+                            })}
+                        />
+                        {errors.email && (
+                            <span className="error-form">
+                                {errors.email.message}
+                            </span>
+                        )}
 
-                {/* Role 
+                        {/* ROLE 
                 TODO : A compléter automatiquement */}
-                <label htmlFor="role">Role</label>
-                <select
-                    disabled
-                    {...register("role", {
-                        required: "Role obligatoire",
-                    })}
-                >
-                    <option value="user" selected>
-                        User
-                    </option>
-                    <option value="admin">Admin</option>
-                </select>
+                        <label htmlFor="role">Role</label>
+                        <select
+                            disabled
+                            defaultValue="user"
+                            {...register("role")}
+                        >
+                            <option value="user">User</option>
+                            <option value="admin">Admin</option>
+                        </select>
 
-                {/* MOT DE PASSE
+                        {/* MOT DE PASSE
                 TODO : Compléter la vérif password */}
-                <label htmlFor="password">Mot de passe</label>
-                <input
-                    type="password"
-                    placeholder="mot de passe"
-                    {...register("password", {
-                        required: "password obligatoire",
-                    })}
-                />
-                {errors.password && (
-                    <span className="error-form">
-                        {errors.password.message}
-                    </span>
-                )}
+                        <label htmlFor="password">Mot de passe</label>
+                        <input
+                            type="password"
+                            placeholder="mot de passe"
+                            {...register("password", {
+                                required: "Mot de passe obligatoire",
+                            })}
+                        />
+                        {errors.password && (
+                            <span className="error-form">
+                                {errors.password.message}
+                            </span>
+                        )}
 
-                {/* CONFIRME MOT DE PASSE
+                        {/* CONFIRME MOT DE PASSE
                 TODO : Compléter la vérif password */}
-                <label htmlFor="passwordVerif">Confirmer le mot de passe</label>
-                <input
-                    type="password"
-                    placeholder="Confirmer"
-                    {...register("passwordVerif", {
-                        required: "Champs obligatoire",
-                    })}
-                />
-                {errors.passwordVerif && (
-                    <span className="error-form">
-                        {errors.passwordVerif.message}
-                    </span>
-                )}
+                        <label htmlFor="passwordVerif">
+                            Confirmer le mot de passe
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="Confirmer votre mot de passe"
+                            {...register("passwordVerif", {
+                                required: "Champs obligatoire",
+                            })}
+                        />
+                        {errors.passwordVerif && (
+                            <span className="error-form">
+                                {errors.passwordVerif.message}
+                            </span>
+                        )}
 
-                <Button type={"submit"} className={"btn-primary-2"}>
-                    Valider
-                </Button>
-                {isSubmitSuccessful && <h1>{watch("pseudo")}</h1>}
-            </form>
+                        <Button type={"submit"} className={"btn-primary-2"}>
+                            Valider
+                        </Button>
+                    </form>
+                </>
+            )}
         </>
     );
 };
