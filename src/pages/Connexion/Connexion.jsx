6cd6d72959361e65b3a9706b8e417b7f149df5ef
bruly2 <b.bertogal@gmail.com>
2023-08-29 02:39:19 +0200

    Update Connexion content

--- src/pages/Connexion/Connexion.jsx
+++ src/pages/Connexion/Connexion.jsx
@@ -17,7 +17,7 @@ const Connexion = () => {
         register,
         handleSubmit,
         setError,
-        formState: { errors, isSubmitSuccessful },
+        formState: { errors },
     } = useForm({ mode: "onTouched" });
 
     const onSubmit = async (data) => {
@@ -45,97 +45,84 @@ const Connexion = () => {
             setError("root.serverError", {
                 type: "server",
                 message:
-                    "Mauvais identifiant ou mot de passe, merci de vous reconnecter",
+                    "L'identifiant ou mot de passe sont incorrects, merci de vous reconnecter",
             });
         }
     };
 
     return (
         <>
-            {isSubmitSuccessful ? (
-                <>
-                    {/* TODO Redirection vers le hub */}
-                    <h1>TODO : Redirection vers le HUB</h1>
-                </>
-            ) : (
-                <>
-                    <h1>Connexion</h1>
-                    <h2>Participer à votre événement</h2>
+            <h1>Connexion</h1>
+            <h2>Participer à votre événement</h2>
 
-                    <main id="connexion">
-                        <form
-                            className="connexion"
-                            onSubmit={handleSubmit(onSubmit)}
+            <main id="connexion">
+                <motion.form
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                    className="connexion"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    {isLoading ? (
+                        <motion.div
+                            className="loading"
+                            initial={{ scale: 0 }}
+                            animate={{ scale: 1 }}
                         >
-                            {isLoading ? (
-                                <motion.div
-                                    className="loading"
-                                    initial={{ scale: 0 }}
-                                    animate={{ scale: 1 }}
-                                >
-                                    <h2>Chargement en cours...</h2>
-                                    <Loader />
-                                </motion.div>
-                            ) : (
-                                <div>
-                                    {errors.root?.serverError && (
-                                        <p className="error-form server">
-                                            {errors.root.serverError.message}
-                                        </p>
-                                    )}
-
-                                    {/* PSEUDO */}
-
-                                    <label htmlFor="username">Email</label>
-                                    <input
-                                        type="email"
-                                        placeholder="Adresse email ?"
-                                        {...register("username", {
-                                            required: "Champs obligatoire",
-                                        })}
-                                    />
-                                    {errors.username && (
-                                        <span className="error-form">
-                                            {errors.username.message}
-                                        </span>
-                                    )}
-
-                                    {/* MOT DE PASSE */}
-                                    <label htmlFor="password">
-                                        Mot de passe
-                                    </label>
-                                    <input
-                                        type="password"
-                                        placeholder="mot de passe"
-                                        {...register("password", {
-                                            required:
-                                                "Mot de passe obligatoire",
-                                        })}
-                                    />
-                                    {errors.password && (
-                                        <span className="error-form">
-                                            {errors.password.message}
-                                        </span>
-                                    )}
-
-                                    <Button
-                                        type={"submit"}
-                                        className={"btn-primary-2"}
-                                    >
-                                        Se connecter
-                                    </Button>
-                                </div>
+                            <h2>Connexion en cours...</h2>
+                            <Loader />
+                        </motion.div>
+                    ) : (
+                        <div>
+                            {errors.root?.serverError && (
+                                <p className="error-form server">
+                                    {errors.root.serverError.message}
+                                </p>
                             )}
-                        </form>
-                        <aside>
-                            <h3>Pas encore de compte&nbsp;?</h3>
-                            <Button className={"btn-secondary"} type={"button"}>
-                                <Link to="/inscription">Créer un compte</Link>
+
+                            {/* PSEUDO */}
+
+                            <label htmlFor="username">Email</label>
+                            <input
+                                type="email"
+                                placeholder="Adresse email ?"
+                                {...register("username", {
+                                    required: "Champs obligatoire",
+                                })}
+                            />
+                            {errors.username && (
+                                <span className="error-form">
+                                    {errors.username.message}
+                                </span>
+                            )}
+
+                            {/* MOT DE PASSE */}
+                            <label htmlFor="password">Mot de passe</label>
+                            <input
+                                type="password"
+                                placeholder="mot de passe"
+                                {...register("password", {
+                                    required: "Mot de passe obligatoire",
+                                })}
+                            />
+                            {errors.password && (
+                                <span className="error-form">
+                                    {errors.password.message}
+                                </span>
+                            )}
+
+                            <Button type={"submit"} className={"btn-primary-2"}>
+                                Se connecter
                             </Button>
-                        </aside>
-                    </main>
-                </>
-            )}
+                        </div>
+                    )}
+                </motion.form>
+                <aside>
+                    <h3>Pas encore de compte&nbsp;?</h3>
+                    <Button className={"btn-secondary"} type={"button"}>
+                        <Link to="/inscription">Créer un compte</Link>
+                    </Button>
+                </aside>
+            </main>
         </>
     );
 };
