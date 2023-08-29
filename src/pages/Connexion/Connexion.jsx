199a477f010894787df60d12403e67a4d21391dc
christophehascoet <yoda19712022@outlook.fr>
2023-08-29 15:18:10 +0200

    correction

--- src/pages/Connexion/Connexion.jsx
+++ src/pages/Connexion/Connexion.jsx
@@ -7,125 +7,125 @@ import Loader from "../../layout/Loader/Loader";
 import { motion } from "framer-motion";
 
 const Connexion = () => {
-    const navigate = useNavigate();
-    const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
-    // FORM + FETCH
-    // TODO Ajouter le catch d'erreur
+  // FORM + FETCH
+  // TODO Ajouter le catch d'erreur
 
-    const {
-        register,
-        handleSubmit,
-        setError,
-        formState: { errors },
-    } = useForm({ mode: "onTouched" });
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({ mode: "onTouched" });
 
-    const onSubmit = async (data) => {
-        setIsLoading(true);
-        const response = await fetch(
-            `${import.meta.env.VITE_API_BASE_URL}/login_check`,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            }
-        );
-        const result = await response.json();
-        // response.headers.common.Authorization = `bearer ${result.token}`;
-        if (response.status === 200) {
-            console.log(`Connexion réussie !! 🥳🥳 Récupération du token :`);
-            console.log(result.token);
-            localStorage.setItem("token", result.token);
-            setIsLoading(false);
-            navigate("/hub");
-        } else {
-            setIsLoading(false);
-            setError("root.serverError", {
-                type: "server",
-                message:
-                    "L'identifiant ou mot de passe sont incorrects, merci de vous reconnecter",
-            });
-        }
-    };
-
-    return (
-        <>
-            <h1>Connexion</h1>
-            <h2>Participer à votre événement</h2>
-
-            <main id="connexion">
-                <motion.form
-                    initial={{ scale: 0 }}
-                    animate={{ scale: 1 }}
-                    className="connexion"
-                    onSubmit={handleSubmit(onSubmit)}
-                >
-                    {isLoading ? (
-                        <motion.div
-                            className="loading"
-                            initial={{ scale: 0 }}
-                            animate={{ scale: 1 }}
-                        >
-                            <h2>Connexion en cours...</h2>
-                            <Loader />
-                        </motion.div>
-                    ) : (
-                        <div>
-                            {errors.root?.serverError && (
-                                <p className="error-form server">
-                                    {errors.root.serverError.message}
-                                </p>
-                            )}
-
-                            {/* PSEUDO */}
-
-                            <label htmlFor="username">Email</label>
-                            <input
-                                type="email"
-                                aria-invalid={errors.email ? "true" : "false"}
-                                placeholder="Adresse email ?"
-                                {...register("username", {
-                                    required: "Champs obligatoire",
-                                })}
-                            />
-                            {errors.username && (
-                                <span className="error-form" role = "alert">
-                                    {errors.username.message}
-                                </span>
-                            )}
-
-                            {/* MOT DE PASSE */}
-                            <label htmlFor="password">Mot de passe</label>
-                            <input
-                                type="password"
-                                placeholder="mot de passe"
-                                aria-invalid={errors.password ? "true" : "false"}
-                                {...register("password", {
-                                    required: "Mot de passe obligatoire",
-                                })}
-                            />
-                            {errors.password && (
-                                <span className="error-form" role ="alert">
-                                    {errors.password.message}
-                                </span>
-                            )}
-
-                            <Button type={"submit"} className={"btn-primary-2"}>
-                                Se connecter
-                            </Button>
-                        </div>
-                    )}
-                </motion.form>
-                <aside>
-                    <h3>Pas encore de compte&nbsp;?</h3>
-                    <Button className={"btn-secondary"} type={"button"}>
-                        <Link to="/inscription">Créer un compte</Link>
-                    </Button>
-                </aside>
-            </main>
-        </>
+  const onSubmit = async (data) => {
+    setIsLoading(true);
+    console.log("test🥳");
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/login_check`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
     );
+    const result = await response.json();
+    if (response.status === 200) {
+      console.log(`Connexion réussie !! 🥳🥳 Récupération du token :`);
+      console.log(result.token);
+      localStorage.setItem("token", result.token);
+      setIsLoading(false);
+      navigate("/hub");
+    } else {
+      setIsLoading(false);
+      setError("root.serverError", {
+        type: "server",
+        message:
+          "L'identifiant ou mot de passe sont incorrects, merci de vous reconnecter",
+      });
+    }
+  };
+
+  return (
+    <>
+      <h1>Connexion</h1>
+      <h2>Participer à votre événement</h2>
+
+      <main id="connexion">
+        <motion.form
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          className="connexion"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          {isLoading ? (
+            <motion.div
+              className="loading"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+            >
+              <h2>Connexion en cours...</h2>
+              <Loader />
+            </motion.div>
+          ) : (
+            <div>
+              {errors.root?.serverError && (
+                <p className="error-form server">
+                  {errors.root.serverError.message}
+                </p>
+              )}
+
+              {/* PSEUDO */}
+
+              <label htmlFor="username">Email</label>
+              <input
+                type="email"
+                aria-invalid={errors.email ? "true" : "false"}
+                placeholder="Adresse email ?"
+                {...register("username", {
+                  required: "Champs obligatoire",
+                })}
+              />
+              {errors.username && (
+                <span className="error-form" role="alert">
+                  {errors.username.message}
+                </span>
+              )}
+
+              {/* MOT DE PASSE */}
+              <label htmlFor="password">Mot de passe</label>
+              <input
+                type="password"
+                placeholder="mot de passe"
+                aria-invalid={errors.password ? "true" : "false"}
+                {...register("password", {
+                  required: "Mot de passe obligatoire",
+                })}
+              />
+              {errors.password && (
+                <span className="error-form" role="alert">
+                  {errors.password.message}
+                </span>
+              )}
+
+              <Button type={"submit"} className={"btn-primary-2"}>
+                Se connecter
+              </Button>
+            </div>
+          )}
+        </motion.form>
+        <aside>
+          <h3>Pas encore de compte&nbsp;?</h3>
+          <Button className={"btn-secondary"} type={"button"}>
+            <Link to="/inscription">Créer un compte</Link>
+          </Button>
+        </aside>
+      </main>
+    </>
+  );
 };
 export default Connexion;
