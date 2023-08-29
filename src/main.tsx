f12d0a41d2dc0d0b4090cf88a61c1dc740ccd3ee
bruly2 <b.bertogal@gmail.com>
2023-08-29 16:51:35 +0200

    Initialisation TSX

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App.jsx";
+import App from "./components/App/App.tsx";
 import "./assets/styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
 import ConnectedProvider from "./utils/context/ConnectedProvider.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <ConnectedProvider>
             <App />
